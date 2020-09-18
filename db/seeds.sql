fdd27533e7bf07cdd3900ba1e63268965af27e11
danybb2020 <daniel.balderas@gmail.com>
2020-09-18 14:24:15 -0700

    First upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('burger max');
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Everything burger', true);
+INSERT INTO burgers (burger_name) VALUES ('All powerful burger');
